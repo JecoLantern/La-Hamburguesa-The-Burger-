918cbea4746b07c88f0db577aab52c4f5490d814
JecoLantern <jbayonke@hotmail.com>
2018-11-26 20:17:09 -0800

    config and controllers

---
+++ config/orm.js
@@ -0,0 +1,35 @@
+var mysql = require('mysql');
+var connection = require('./connection.js');
+
+module.exports = {
+    selectAll: function(table, orderBy, direction, callback) {
+        var queryString = 'SELECT * FROM ?? ORDER BY ??;';
+        connection.query(queryString, [table, orderBy, direction], function(err,data) {
+            if (err) {
+                return console.log(err);
+            }else{
+                return callback(data);
+            }
+        });
+    },
+    insertOne: function(table, columns, values, callback) {
+        var queryString = 'INSERT INTO ' + table + columns + ' VALUES (' + values + ')';
+        connection.query(queryString, function(err, data) {
+            if (err) {
+                return console.log(err);
+            }else{
+                return callback(data);
+            }
+        });
+    },
+    updateOne: function(table, properties, selector, callback){
+        var queryString = 'UPDATE ' + table + ' SET ' + properties + ' WHERE ' + selector;
+        connection.query(queryString, function(err, data) {
+            if (err) {
+                return console.log(err);
+            }else {
+                return callback(data);
+            }
+        });
+    }
+};
---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = ('path');
+var exphbs = require('express-handlebars');
+var favicon = require('serve-favicon');
+var methodOverride = require('method-override');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+var PORT = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(favicon(path.join(__dirname, 'public/assets/img', 'burger.png')));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use(methodOverride('_method'));
+
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
+
+app.listen(PORT);
