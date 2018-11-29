da2c3034d945dcf2d56ca08413de428c1ff3bab9
JecoLantern <jbayonke@hotmail.com>
2018-11-28 18:06:26 -0800

    sql, jses,handlebars

---
+++ models/burger.js
@@ -0,0 +1,25 @@
+var orm = require('../config/orm.js');
+
+module.exports = {
+    all: function(callback) {
+        orm.selectAll('burgers', 'id', 'DESC', function(data) {
+            callback(data);
+        });
+    },
+    post: function(input, callback) {
+        var columns = '(burger_name, devoured)';
+        var values = '\'' + input + '\', false';
+
+        orm.insertOne('burgers', columns, values, function(data) {
+            callback(data);
+        });
+    },
+    update: function(property, selector, callback) {
+        var update = 'devoured = ' + property;
+        var condition = 'id = ' + selector;
+
+        orm.updateOne('burgers', updatee, condition, function(data) {
+            callback(data);
+        })
+    }
+};
